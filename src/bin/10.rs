b7ac5dab69fe0f0400b96ac7fc4822d5fb71841a
plastic041 <plastic041@gmail.com>
2022-12-10 17:25:42 +0900

    struct

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -6,6 +6,53 @@ fn should_calc(cycles: u32) -> bool {
     cycles >= 20 && (cycles - 20) % 40 == 0
 }
 
+struct Cpu {
+    cycles: i32,
+    register: i32,
+    crt_position: i32,
+
+    crt: Vec<char>,
+}
+
+impl Cpu {
+    fn new() -> Self {
+        Cpu {
+            cycles: 1,
+            register: 1,
+            crt_position: 0,
+
+            crt: vec![],
+        }
+    }
+
+    fn draw(&mut self) {
+        if self.crt_position == self.register - 1
+            || self.crt_position == self.register
+            || self.crt_position == self.register + 1
+        {
+            self.crt.push('#');
+        } else {
+            self.crt.push('.');
+        }
+
+        self.crt_position += 1;
+
+        if self.cycles % 40 == 0 {
+            println!("{}", self.crt.iter().collect::<String>());
+            self.crt.clear();
+            self.crt_position = 0;
+        }
+    }
+
+    fn tick(&mut self) {
+        self.cycles += 1;
+    }
+
+    fn update(&mut self, value: i32) {
+        self.register += value;
+    }
+}
+
 pub fn part_one(input: &str) -> Option<i32> {
     let lines = input.lines();
 
@@ -44,69 +91,23 @@ pub fn part_one(input: &str) -> Option<i32> {
 pub fn part_two(input: &str) -> Option<i32> {
     let lines = input.lines();
 
-    let mut cycles: i32 = 1;
-    let mut register: i32 = 1;
-    let mut crt_position: i32 = 0;
-
-    let mut crt: Vec<String> = vec![];
+    let mut cpu = Cpu::new();
 
     for line in lines {
         if line == "noop" {
-            if crt_position == register - 1
-                || crt_position == register
-                || crt_position == register + 1
-            {
-                crt.push("#".to_string());
-            } else {
-                crt.push(".".to_string());
-            }
-            crt_position += 1;
-            if cycles % 40 == 0 {
-                println!("{}", crt.join(""));
-                crt.clear();
-                crt_position = 0;
-            }
-
-            cycles += 1;
+            cpu.draw();
+            cpu.tick();
         } else {
             let mut split = line.split_whitespace();
             let _ = split.next();
             let value = split.next().unwrap().parse::<i32>().unwrap();
 
-            if crt_position == register - 1
-                || crt_position == register
-                || crt_position == register + 1
-            {
-                crt.push("#".to_string());
-            } else {
-                crt.push(".".to_string());
-            }
-            crt_position += 1;
-            if cycles % 40 == 0 {
-                println!("{}", crt.join(""));
-                crt.clear();
-                crt_position = 0;
-            }
+            cpu.draw();
+            cpu.tick();
 
-            cycles += 1;
-
-            if crt_position == register - 1
-                || crt_position == register
-                || crt_position == register + 1
-            {
-                crt.push("#".to_string());
-            } else {
-                crt.push(".".to_string());
-            }
-            crt_position += 1;
-            if cycles % 40 == 0 {
-                println!("{}", crt.join(""));
-                crt.clear();
-                crt_position = 0;
-            }
-            cycles += 1;
-
-            register += value;
+            cpu.draw();
+            cpu.tick();
+            cpu.update(value);
         }
     }
 
